56570bab016d77fc958c06153fee80d0b3d726f4
Thivakar-T <thivakardravid1508@gmail.com>
2022-12-07 17:00:42 +0530

    merge

--- src/materials/materials-routing.module.ts
+++ src/materials/materials-routing.module.ts
@@ -7,4 +7,6 @@ const routes: Routes = [];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MaterialsRoutingModule { }
+export class MaterialsRoutingModule {
+  
+ }
