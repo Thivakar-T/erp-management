f2b5530879b0fc6e222a114d4fe87becf8ca39bc
keerpugal <111882650+keerpugal@users.noreply.github.com>
2022-12-07 17:37:36 +0530

    changes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,9 @@
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
-import { LayoutModule } from 'src/layout/layout.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { AddUserComponent } from './add-user/add-user.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
-import { MaterialsModule } from './../../materials/materials.module';
-
 @NgModule({
   declarations: [
     AddUserComponent,
@@ -17,10 +13,8 @@ import { MaterialsModule } from './../../materials/materials.module';
   imports: [
     CommonModule,
     PagesRoutingModule,
-    LayoutModule,
-    MaterialsModule,
-    ReactiveFormsModule,
-    FormsModule
   ]
+  
+  
 })
 export class PagesModule { }
