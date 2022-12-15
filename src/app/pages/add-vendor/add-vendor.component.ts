b279a5848e3c99e7c1c3127eae18599b5f167e16
123ramanathan <ngksriram23111@gmail>
2022-12-15 15:08:15 +0530

    change

--- src/app/pages/add-vendor/add-vendor.component.ts
+++ src/app/pages/add-vendor/add-vendor.component.ts
@@ -13,17 +13,10 @@ interface cate {
   styleUrls: ['./add-vendor.component.scss']
 })
 export class AddVendorComponent implements OnInit {
-  formpage!: FormGroup;
+  studentForm!: FormGroup;
   public Submitted = false;
-  ReadMore: boolean = true
-  visible: boolean = false
-
-  classes: any
-  sections: any
-  months: any
-  years: any
-  status:any
-
+  obj: any = {};
+  selectedCar!: number;
   country= [
     {id: 1, name: 'Vilnius'},
     {id: 2, name: 'Kaunas'},
@@ -45,58 +38,48 @@ state = [
   {id: 4, name: 'Pabradė'},
   {id: 5, name: 'Klaipėda'}
 ];
-cate: cate[] = [
-  { value: 'Normal-0', viewValue: 'Normal' },
-  { value: 'OPPOSITIONAL DEFIANT DISORDER-1', viewValue: 'Normal' },
-  { value: 'ANXIETY DISORDER-2', viewValue: 'OPPOSITIONAL DEFIANT DISORDER' },
-  { value: 'CONDUCT DISORDER-3', viewValue: 'ANXIETY DISORDER' },
-  { value: 'OBSESSIVE COMPULSIVE  DISORDER -4', viewValue: 'OBSESSIVE COMPULSIVE  DISORDER' },
-  { value: 'ATTENTION DEFICIT HYPERACTIVITY DISORDER-5', viewValue: 'ATTENTION DEFICIT HYPERACTIVITY DISORDER' },
 
-
+Status = [
+  {id: 1, name: 'Active'},
+  {id: 2, name: 'Inactive'},
+ 
 ];
-  constructor(private fb: FormBuilder, private route: Router,
-    private router: ActivatedRoute) {
-    
-  }
 
+
+constructor(private fb: FormBuilder,
+  private route: Router,
+  private router: ActivatedRoute){}
+  
   ngOnInit(): void {
-    this.formpage = this.fb.group({
+    this.studentForm = this.fb.group({
+     
+     
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      emailAddress: ['', Validators.required],
-      PhoneNo: ['', Validators.required],
+      emailAddress: ['',[Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      PhoneNo: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),Validators.maxLength(10),Validators.minLength(10)]],
       CountryId: ['', Validators.required],
       StateId: ['', Validators.required],
       CityId: ['', Validators.required],
-      gstIN: ['', Validators.required],
+      gstin: ['', Validators.required],
       Status: ['', Validators.required],
       AddressLine1: ['', Validators.required],
       AddressLine2: ['', Validators.required],
-      Note: ['', Validators.required],
+      note: ['', Validators.required],
      
-    });
-
-   this.status=[
-    "Active","InActive"
-   ]
-
-
+      
+     
+    }) 
   }
+  
   get f(): { [key: string]: AbstractControl } {
-    return this.formpage.controls;
+    return this.studentForm.controls;
+  }
+  onSubmit() {
+    this.Submitted = true;
   }
 
-  onsubmit() {
-   this.Submitted = true;
-      console.log(this.formpage.value);
-
-      this.ReadMore = !this.ReadMore;
-      this.visible = !this.visible
-    
   }
-
-}
 
 
 
